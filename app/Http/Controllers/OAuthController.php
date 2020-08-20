3722ecb40f9e3cccb24b3cb215093758a2c69bf5
石田裕 <y.ishida.carecon@gmail.com>
2020-08-20 13:49:57 +0900

    Slackログインテスト追加

---
+++ app/Http/Controllers/OAuthController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Http\Controllers;
+use Socialite;
+class OAuthController extends Controller
+{
+
+    // Slack認証へ遷移
+    public function redirectToProvider()
+    {
+        return Socialite::driver('slack')->redirect();
+    }
+    // Slackからのリダイレクト
+    public function handleProviderCallback()
+    {
+        $user = Socialite::driver('slack')->stateless()->user();
+        return view('login2', ['user' => $user]);
+    }
+}
