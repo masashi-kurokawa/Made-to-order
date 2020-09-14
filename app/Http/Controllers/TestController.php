013150c39942efa35b6348a93f863cfa6953ac1e
黒川将志 <m.kurokawa.carecon@gmail.com>
2020-09-14 16:03:16 +0900

    テスト受講ページ（CRUD処理）取り組み中

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,31 +9,76 @@ use Illuminate\Support\Facades\DB;
 class TestController extends Controller
 {
     //
-    public function index()
+    public function index(Request $request)
     {
 
-       //POSTされてきた情報
 
-       //どのテストか？
-
-
+    // テスト表示処理　始まり
        //DBから情報持ってくる
        $test = new Test;
        //表示したいテストID持ってくる
-       $testnumber = $test->where('id',1)->value('id'); //('id',1)の中の１は変数になる
-       $items = DB::table('select_tests','hole_tests','write_tests',)
-       // $items = DB::table('hole_tests','write_tests',)
-       ->whereTest_id("$testnumber")->get();
+       $testnumber = $test->where('id',1)->value('id'); //('id',1)の中の１は変数になる POSTされてきた
+       $selectitems = DB::table('select_tests')->whereTest_id("$testnumber")->get()->toArray();
+       $holeitems = DB::table('hole_tests')->whereTest_id("$testnumber")->get()->toArray();
+       $writeitems = DB::table('write_tests')->whereTest_id("$testnumber")->get()->toArray();
 
-       //ここからテスト表示処理
+       //配列を合わせる
+       $str3 = array_merge($selectitems, $holeitems, $writeitems);
+
+       // collect（数字化？）
+       $sorted = collect($str3);
+
+       // テストのソート
+       $sorteds = $sorted->sortBy('question_number')->all();
+    // テスト表示処理　終わり
 
 
 
-       // $dblist = $query->get();
-       dump($items);
+    // 　テストの答え格納　始まり
+       //POSTされてきた情報
+       $answers = $request->all(); //GETで送られてきたデータ
+       // 何問あるか？
+       $count = $sorted->count('question_number');
+
+       // $testanswer1 = $request->input('answer1'); //回答
+       // $testanswer11 = $request->input('1'); //答え
+
+       // for ($i = 1; $i <= 10; $i++){
+       for ($i = 1; $i <= "{$count}"; $i++){
+         $testanswer[$i] = $request->input("answer$i"); //回答
+         $testanswer1[$i] = $request->input("$i"); //答え
+         dump($testanswer[$i]);
+         dump($testanswer1[$i]);
+       };
+       // $testanswer11 = $request->input('qa',"{question_number}"); //答え
+
+       // $testans = "{$testanswer1}";
+
+       // 問題が正解か判定する
+
+
+
+
+
+
+
+
+    // 　テストの答え格納　終わり
+
+
+
+
+       // dump($sorteds->toArray());
+       // var_dump($sorted);
+       // dump($testanswer11);
+       // dump($testanswer1);
+       dump($answers);
+       // dump($count);
+       // dump(is_array($answers)); //配列か確認
 
        // return view('test');
-       return view('test',compact('items'));
+       return view('test',compact('sorteds'));
+       // return view('test',compact('sorted', 'sub_value'));
        // return view('test',compact('', '', '', ''));
     }
 }
