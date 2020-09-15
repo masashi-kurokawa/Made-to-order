ade0b2ea8467ec5f2604cb351511650dd02389ab
niwakazumasa <khn.kazmasa@gmail.com>
2020-09-15 13:59:55 +0900

    マイグレーション修正

--- database/migrations/2020_09_02_100351_create_write_survey_answers_table.php
+++ database/migrations/2020_09_02_100351_create_write_survey_answers_table.php
@@ -23,7 +23,7 @@ class CreateWriteSurveyAnswersTable extends Migration
             $table->timestamps();
 
             // 外部キーの設定
-            $table->foreign('user_id')->references('id')->on('users');
+           // $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
