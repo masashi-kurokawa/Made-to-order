bec79c3c75713edef57169c6138cbd9718a0447f
黒川将志 <m.kurokawa.carecon@gmail.com>
2020-09-27 10:07:26 +0900

    テスト新規作成完了

--- app/Http/Controllers/TestcreateController.php
+++ app/Http/Controllers/TestcreateController.php
@@ -126,31 +126,31 @@ class TestcreateController extends Controller
             //   'updated_at' => Carbon::now(),  //時間が違う、場所の設定が違うのかも
             // ]);
           } elseif ($text8[$i]) {
-            DB::table('select_tests')->insert([
-              'test_id' => "$testno", //ok
-              'question' => "$text8[$i]", //ok
-              'answer' => "$answer", // ok
-              'select1' => "1", // ok
-              'select2' => "2", //
-              'select3' => "3", //
-              'select4' => "4", //
-              'select5' => "5", //
-              'select6' => "6", //
-              'select7' => "7", //
-              'select8' => "8", // ok
-              'select_item1' => "$answer8_1[$i]", // ok
-              'select_item2' => "$answer8_2[$i]", //
-              'select_item3' => "$answer8_3[$i]", //
-              'select_item4' => "$answer8_4[$i]", //
-              'select_item5' => "$answer8_5[$i]", //
-              'select_item6' => "$answer8_6[$i]", //
-              'select_item7' => "$answer8_7[$i]", //
-              'select_item8' => "$answer8_8[$i]", // ok
-              'question_number' => "$question_number[$i]", //ok
-              'role' => '1',  //ロール権限 洗濯問題 ok
-              'created_at' => Carbon::now(), //時間が違う、場所の設定が違うのかも
-              'updated_at' => Carbon::now(),  //時間が違う、場所の設定が違うのかも
-            ]);
+            // DB::table('select_tests')->insert([
+            //   'test_id' => "$testno", //ok
+            //   'question' => "$text8[$i]", //ok
+            //   'answer' => "$answer", // ok
+            //   'select1' => "1", // ok
+            //   'select2' => "2", //
+            //   'select3' => "3", //
+            //   'select4' => "4", //
+            //   'select5' => "5", //
+            //   'select6' => "6", //
+            //   'select7' => "7", //
+            //   'select8' => "8", // ok
+            //   'select_item1' => "$answer8_1[$i]", // ok
+            //   'select_item2' => "$answer8_2[$i]", //
+            //   'select_item3' => "$answer8_3[$i]", //
+            //   'select_item4' => "$answer8_4[$i]", //
+            //   'select_item5' => "$answer8_5[$i]", //
+            //   'select_item6' => "$answer8_6[$i]", //
+            //   'select_item7' => "$answer8_7[$i]", //
+            //   'select_item8' => "$answer8_8[$i]", // ok
+            //   'question_number' => "$question_number[$i]", //ok
+            //   'role' => '1',  //ロール権限 洗濯問題 ok
+            //   'created_at' => Carbon::now(), //時間が違う、場所の設定が違うのかも
+            //   'updated_at' => Carbon::now(),  //時間が違う、場所の設定が違うのかも
+            // ]);
           }
 
         }
