74e4d7e98ebacf8bf6ccff55e6dce90e1d4ccfcd
黒木零央 <r.kuroki.carecon@gmail.com>
2020-09-27 12:36:41 +0900

    テスト詳細編集の修正(途中)

--- app/Http/Controllers/TesteditController.php
+++ app/Http/Controllers/TesteditController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Test;
 use App\Models\Write_test;
 use App\Models\Select_test;
+use App\Models\Hole_test;
 use Illuminate\Support\Facades\DB;
 
 class TesteditController extends Controller
@@ -37,5 +38,58 @@ class TesteditController extends Controller
           'test' => $test,
           'test_id' => $id,
         ], compact('sort'));
+    }
+
+    public function editRegister($id, Request $request)
+    {
+        $test = new Test;
+        $write_test = new Write_test;
+        $select_test = new Select_test;
+        $hole_test = new Hole_test;
+
+        // 削除処理
+        $write_test_data = $write_test->where('test_id', $id)->delete();
+        $select_test_data = $select_test->where('test_id', $id)->delete();
+        $hole_test_data = $hole_test->where('test_id', $id)->delete();
+        $test_data = $test->where('id', $id)->delete();
+
+        foreach ($request->request as $key => $value) {
+            // $requestの'_token'以外を使う
+            if ($key != '_token') {
+                // view側でroleの値も取得できるようにする
+                if ('role == 1') {
+                    $write_test->test_id = $id;
+                    $write_test->question = $key->question;
+                    $write_test->question_number = $key->question_number;
+                    $write_test->save();
+
+                }elseif ('role == 2') {
+                    $select_test->test_id = $id;
+                    $select_test->question = $key->question;
+                    $select_test->answer = $key->answer;
+                    $select_test->select_item1 = $key->Choice1;
+                    $select_test->select_item2 = $key->Choice2;
+                    $select_test->select_item3 = $key->Choice3;
+                    $select_test->select_item4 = $key->Choice4;
+                    // 8択問題だった時に追加する処理
+                    if (array_key_exists( 'Choice5', $key )) {
+                        $select_test->select_item5 = $key->Choice5;
+                        $select_test->select_item6 = $key->Choice6;
+                        $select_test->select_item7 = $key->Choice7;
+                        $select_test->select_item8 = $key->Choice8;
+                    }
+                    $select_test->question_number = $key->question_number;
+                    $select_test->save();
+
+                }elseif ('role == 3') {
+                    $hole_test->test_id = $id;
+                    $hole_test->question = $key->question;
+                    $hole_test->answer1 = $key->answer1;
+                    $hole_test->answer2 = $key->answer2;
+                    $hole_test->question_number = $key->question_number;
+                    $hole_test->save();
+                }
+            }
+        }
     }
 }
